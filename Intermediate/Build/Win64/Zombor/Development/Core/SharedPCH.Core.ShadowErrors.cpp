e215b5cb781684bc64b66d7127c09c60b313b513
HiImWhite <wojttasek1999@gmail.com>
2023-01-10 15:32:53 +0100

    Finishing game, few files are too big.

---
+++ Intermediate/Build/Win64/Zombor/Development/Core/SharedPCH.Core.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30145
+#include "C:/Users/wojta/Documents/GitHub/Zombor/Intermediate/Build/Win64/Zombor/Development/Core/SharedPCH.Core.ShadowErrors.h"
---
+++ Intermediate/Source/Zombor.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Zombor : ModuleRules
+{
+	public Zombor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
